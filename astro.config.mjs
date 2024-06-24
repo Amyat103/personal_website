e9708b1982cb4805b685ce000dc54bc4cc9b0404
Amyat103 <zhoudavid7312@gmail.com>
2024-06-24 16:25:43 -0400

    fix file path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://Amyat103.github.io/personal_website/',
+  base: '/personal_website/',
   outDir: './docs',
   integrations: [mdx(), sitemap(), tailwind()],
 });
