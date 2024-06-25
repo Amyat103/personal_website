ec161629bc3d8b69a52aca75319b2c9f79541953
Amyat103 <zhoudavid7312@gmail.com>
2024-06-25 18:57:55 -0400

    Add detailed debugging steps for build process

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://Amyat103.github.io',
+  site: 'https://Amyat103.github.io/personal_website',
   base: '/personal_website',
+  output: 'static',
 });
