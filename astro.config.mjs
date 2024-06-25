49cdb485adf5c4e6bbdc3dc452bda17f187309c0
Amyat103 <zhoudavid7312@gmail.com>
2024-06-25 17:28:58 -0400

    Update Astro configuration for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,6 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://amyat103.github.io/personal_website',
+  site: 'https://amyat103.github.io',
   base: '/personal_website/',
-  integrations: [mdx(), sitemap(), tailwind()],
-  build: {
-    outDir: 'docs',
-  },
 });
